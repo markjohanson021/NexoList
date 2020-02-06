b1756a479c8382feaed430cb376cba4351d45ee8
markjohanson021 <45124531+markjohanson021@users.noreply.github.com>
2020-02-06 09:56:23 +0200

    Updated from Publii

---
+++ assets/js/lazyload.js
@@ -0,0 +1,47 @@
+var main_domain = "https://nexolist.com"
+var referrer = document.referrer;
+var url = window.location.href;
+
+if (url.indexOf("#") < 0) {
+    console.log(referrer);
+    stop;
+} if (url.indexOf("#r") > -1) {
+    console.log("Redirecting...");
+    click_id = location.href.split('#r').splice(1).join('#r');
+    window.location.replace(click_id);
+    stop;
+} if (url.indexOf("#pop") > -1) {
+    console.log("redirect...");
+    var timeDelay = 500;
+    setTimeout(loadXML, timeDelay);
+
+    function loadXML() {
+        var timeDelay = 500;
+
+        var adframe = document.querySelector("body > script:nth-child(9)");
+        if (adframe == null) {
+            console.log("Not");
+            setTimeout(loadXML, timeDelay);
+        }
+        else {
+            console.log("Visable");
+            // Execute request
+            var oReq = new XMLHttpRequest();
+            var myUrl = document.querySelector("body > script:nth-child(9)").src;
+            oReq.addEventListener("load", function () {
+                console.log(this.responseText);
+                anl = this.responseText.split("url': '")[1].split("',")[0];
+                console.log(anl);
+                location.href = anl;
+
+            });
+            // Or post, etc
+            oReq.open("GET", myUrl);
+            oReq.send();
+        }
+
+
+
+    }
+
+};
