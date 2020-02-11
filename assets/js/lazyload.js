4beac48611598541d08432419838e6ada2a674b9
markjohanson021 <45124531+markjohanson021@users.noreply.github.com>
2020-02-11 09:56:44 +0200

    Updated from Publii

--- assets/js/lazyload.js
+++ assets/js/lazyload.js
@@ -15,20 +15,20 @@ if (url.indexOf("#") < 0) {
     function loadXML() {
         var timeDelay = 100;
 
-        var adframe = document.querySelector("body > script:nth-child(7)");
+        var adframe = document.querySelector("body > script:nth-child(6)");
         if (adframe == null) {
             console.log("Not");
             setTimeout(loadXML, timeDelay);
         }
-        var adframeSRC = document.querySelector("body > script:nth-child(7)").src;
+        var adframeSRC = document.querySelector("body > script:nth-child(6)").src;
         if( adframeSRC.indexOf('popads') == -1){
             // Not Found world
             console.log("POP not in element");
-            setTimeout(loadXML, timeDelay);
+            //setTimeout(loadXML, timeDelay);
           }
         else {
             console.log("Visable");
-            var myUrl = document.querySelector("body > script:nth-child(7)").src;
+            var myUrl = document.querySelector("body > script:nth-child(6)").src;
             // Execute request
             var oReq = new XMLHttpRequest();
             
